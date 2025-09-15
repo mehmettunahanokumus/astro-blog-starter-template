25367c571c33fa69b8066d8a50948506e0d235c4
Mehmet Tunahan Okumus <80225796+mehmettunahanokumus@users.noreply.github.com>
2025-09-15 18:55:27 +0300

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Secure Future";
+export const SITE_DESCRIPTION = "Exploring the latest in cybersecurity, privacy, and digital protection technologies";
